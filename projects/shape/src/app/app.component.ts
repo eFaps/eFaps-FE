eb0771fe3433e93aa9a9f4aa738273d98ee096a9
Jan Moxter <jan.moxter@moxter.net>
2020-04-24 14:01:16 -0500

    Adding shaoe

--- projects/shape/src/app/app.component.ts
+++ projects/shape/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'eFaps-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "eFaps-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'shape';
+  title = "shape";
 }
