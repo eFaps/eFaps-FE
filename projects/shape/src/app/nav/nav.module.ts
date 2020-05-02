dafb7316afc15ba9c25c036ad282cd59af858916
Jan Moxter <jan.moxter@moxter.net>
2020-05-01 21:59:01 -0500

    Add content wrapper

--- projects/shape/src/app/nav/nav.module.ts
+++ projects/shape/src/app/nav/nav.module.ts
@@ -8,9 +8,10 @@ import { MatIconModule } from "@angular/material/icon";
 import { CardinalComponent } from "./cardinal/cardinal.component";
 import { NavRoutingModule } from "./nav-routing.module";
 import { MenuItemComponent } from './menu-item/menu-item.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
 
 @NgModule({
-  declarations: [CardinalComponent, MenuItemComponent],
+  declarations: [CardinalComponent, MenuItemComponent, ContentWrapperComponent],
   imports: [
     CommonModule,
     NavRoutingModule,
