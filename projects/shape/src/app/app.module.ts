ce17ae4e5bd5f5a9a8ccb465370c24b05b013c5a
Jan Moxter <jan.moxter@moxter.net>
2020-05-02 22:40:15 -0500

    Main toolbar style

--- projects/shape/src/app/app.module.ts
+++ projects/shape/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -7,8 +8,13 @@ import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- projects/shape/src/app/services/nav.service.ts
+++ projects/shape/src/app/services/nav.service.ts
@@ -1,135 +1,143 @@
-import { Injectable } from '@angular/core';
-import { NavItem } from '../models';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { NavItem } from "../models";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class NavService {
   private navSource = new BehaviorSubject<NavItem[]>(null);
   currentNav = this.navSource.asObservable();
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   public load() {
-      this.navSource.next(items);
+    //this.navSource.next(items);
+    this.getNav().subscribe( {
+      next: items => this.navSource.next(items)
+    })
   }
 
-  public loadFromUrl() {
-
+  private getNav(_createNew?: boolean): Observable<NavItem[]> {
+    const requestUrl = `http://localhost:8888/eFaps/servlet/rest/ui/nav`;
+    return this.http.get<NavItem[]>(requestUrl);
   }
+
+  public loadFromUrl() {}
 }
 
-const items : NavItem[] =
-  [
-    {
-      id: "123.4",
-      label: "Configuracion",
-      children: [
-        {
-          id: "123.10",
-          label: "Search",
-        },
-        {
-          id: "123.10",
-          label: "Common",
-          children: [
-            {
-              id: "123.10",
-              label: "DB Properties",
-            },
-            {
-              id: "123.10",
-              label: "Bundles",
-            },
-            {
-              id: "123.10",
-              label: "System Configurations",
-            },
-            {
-              id: "123.10",
-              label: "Versions",
-            }
-          ]
-        },
-        {
-          id: "123.10",
-          label: "User",
-        }
-      ]
-    },
-    {
-      id: "123.5",
-      label: "Contactos",
-      children: [{
+const items: NavItem[] = [
+  {
+    id: "123.4",
+    label: "Configuracion",
+    children: [
+      {
+        id: "123.10",
+        label: "Search"
+      },
+      {
+        id: "123.10",
+        label: "Common",
+        children: [
+          {
+            id: "123.10",
+            label: "DB Properties"
+          },
+          {
+            id: "123.10",
+            label: "Bundles"
+          },
+          {
+            id: "123.10",
+            label: "System Configurations"
+          },
+          {
+            id: "123.10",
+            label: "Versions"
+          }
+        ]
+      },
+      {
+        id: "123.10",
+        label: "User"
+      }
+    ]
+  },
+  {
+    id: "123.5",
+    label: "Contactos",
+    children: [
+      {
         id: "123.5",
-        label: "Buscar Contactos",
+        label: "Buscar Contactos"
       },
       {
         id: "123.5",
-        label: "Contactos por Nombre",
+        label: "Contactos por Nombre"
       }
-      ]
-    },
-    {
-      id: "123.6",
-      label: "Compras",
-      children: [{
+    ]
+  },
+  {
+    id: "123.6",
+    label: "Compras",
+    children: [
+      {
         id: "123.5",
         label: "Gestion de Compras",
-        children: [{
-          id: "123.5",
-          label: "Solicitud de COticasion",
-        }]
+        children: [
+          {
+            id: "123.5",
+            label: "Solicitud de COticasion"
+          }
+        ]
       },
       {
         id: "123.5",
-        label: "Contactos por Nombre",
+        label: "Contactos por Nombre"
       }
-      ]
-    },
-    {
-      id: "123.7",
-      label: "Ventas",
-    },
-    {
-      id: "123.7",
-      label: "Multilevel1",
-      children: [
-        {
-          id: "123.7",
-          label: "Multilevel2",
-          children: [
-            {
-              id: "123.7",
-              label: "Multilevel3",
-            },
-            {
-              id: "123.7",
-              label: "Multilevel3",
-              children: [
-                {
-                  id: "123.7",
-                  label: "Multilevel4",
-                  children: [
-                    {
-                      id: "123.7",
-                      label: "Multilevel5",
-                      children: [
-                        {
-                          id: "123.7",
-                          label: "Multilevel6",
-                          children: [
-
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
+    ]
+  },
+  {
+    id: "123.7",
+    label: "Ventas"
+  },
+  {
+    id: "123.7",
+    label: "Multilevel1",
+    children: [
+      {
+        id: "123.7",
+        label: "Multilevel2",
+        children: [
+          {
+            id: "123.7",
+            label: "Multilevel3"
+          },
+          {
+            id: "123.7",
+            label: "Multilevel3",
+            children: [
+              {
+                id: "123.7",
+                label: "Multilevel4",
+                children: [
+                  {
+                    id: "123.7",
+                    label: "Multilevel5",
+                    children: [
+                      {
+                        id: "123.7",
+                        label: "Multilevel6",
+                        children: []
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
