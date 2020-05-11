872872c1284135f94bd2d923ed8ac8b769d3ad91
Jan Moxter <jan.moxter@moxter.net>
2020-05-10 20:56:24 -0500

    Content module

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ContentRoutingModule } from './content-routing.module';
-import { LayoutComponent } from './layout/layout.component';
-
-
+import { ContentRoutingModule } from "./content-routing.module";
+import { LayoutComponent } from "./layout/layout.component";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [
-    CommonModule,
-    ContentRoutingModule
-  ]
+  imports: [CommonModule, ContentRoutingModule, MatTabsModule]
 })
-export class ContentModule { }
+export class ContentModule {}
