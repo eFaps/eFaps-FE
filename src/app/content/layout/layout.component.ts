61f0ad3da1199e45b8f0a3afd8ff49aa469243e8
Jan Moxter <jan.moxter@moxter.net>
2020-06-04 23:59:18 -0500

    Open close for drawer

--- src/app/content/layout/layout.component.ts
+++ src/app/content/layout/layout.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { NavItem, Content } from "src/app/models";
-import { MatDrawer } from '@angular/material/sidenav';
+import { MatDrawer } from "@angular/material/sidenav";
 
 @Component({
   selector: "eFaps-layout",
   templateUrl: "./layout.component.html",
-  styleUrls: ["./layout.component.scss"],
+  styleUrls: ["./layout.component.scss"]
 })
 export class LayoutComponent implements OnInit, AfterViewInit {
   content: Content;
   tabs: NavItem[] = [];
   selectedIndex = 0;
   overDrawer = false;
-  mode = 'push'
-  drawerWidth = 250
+  mode = "push";
+  drawerWidth = 250;
   isOpen = true;
-  @ViewChild('drawer') drawer: MatDrawer;
+  shouldTrigger = false;
+  @ViewChild("drawer") drawer: MatDrawer;
 
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe({
-      next: (value) => {
-        console.log(value);
+      next: value => {
         this.content = value.content;
-      },
+      }
     });
   }
 
   ngOnInit(): void {
     this.tabs = this.content.nav;
-    this.selectedIndex = this.tabs.findIndex((item) => {
+    this.selectedIndex = this.tabs.findIndex(item => {
       return item.id == this.content.selected;
     });
   }
@@ -38,12 +38,44 @@ export class LayoutComponent implements OnInit, AfterViewInit {
     this.drawer.closedStart.subscribe({
       next: () => {
         this.isOpen = false;
+        this.shouldTrigger = false;
       }
-    })
+    });
     this.drawer.openedStart.subscribe({
       next: () => {
         this.isOpen = true;
       }
-    })
+    });
+  }
+
+  closeDrawer() {
+    this.mode = "push";
+    this.drawer.close();
+  }
+
+  openDrawer() {
+    this.mode = "push";
+    this.drawer.open();
+  }
+
+  leaveDrawer() {
+    if (this.mode == "over") {
+      this.drawer.close();
+    }
+  }
+
+  enterBar() {
+    this.shouldTrigger = true;
+    if (!this.isOpen) {
+      setTimeout(() => {
+        this.mode = "over";
+        this.drawer.open();
+        this.shouldTrigger = false;
+      }, 800);
+    }
+  }
+
+  leaveBar() {
+    this.shouldTrigger = false;
   }
 }
